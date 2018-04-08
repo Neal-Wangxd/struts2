f5ddb59a248f8a5cbb70ba8a091d36a49a9657ff
Neal-Wangxd <wangxd_work@163.com>
2018-04-08 13:49:46 +0800

    book对应 的action

---
+++ struts2-review-page/src/main/java/com/struts/action/BookAction.java
@@ -0,0 +1,20 @@
+package com.struts.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/** 
+* @author 作者 wxd  
+* @version 1.0
+* E-mail: wangxudong@jetsen.cn
+* 创建时间：2018年4月8日 上午11:17:48 
+* 类说明 
+*/
+@SuppressWarnings("serial")
+public class BookAction extends ActionSupport{
+	
+	public String execute(){
+		return "success";
+	}
+
+}
+ 
